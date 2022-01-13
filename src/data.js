ac8cbefcdcb64268eabd3229b57bcd71ebe3c27b
Hoang Minh <minhnhph14989@fpt.edu.vn>
2022-01-13 16:36:47 +0700

    update

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const data = [
     {
         createdAt: "2022-01-10T07:26:52.549Z",
         title: "Vinh danh 295 sinh viên FPT Poly Technic học kì Spring 2018",
-        img: "../image/post.png",
+        img: "../../image/post.png",
         desc: "Tối nay 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc nhất trong học tập cũng như hoạt động",
         id: "1",
     },
