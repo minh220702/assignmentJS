c612393c4ca42ba05593652df0dcd420431c72ff
Hoang Minh <minhnhph14989@fpt.edu.vn>
2022-01-13 23:23:59 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Navigo from "navigo";
 import Detail from "./pages/detail";
 import HomePage from "./pages/home";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
 const router = new Navigo("/", { linksSelector: "a" });
 const print = (content) => {
     document.getElementById("app").innerHTML = content;
@@ -9,6 +11,12 @@ const print = (content) => {
 router.on({
     "/": () => {
         print(HomePage.render());
+    },
+    "/signin" : () => {
+        print(Signin.render());
+    },
+    "/signup" : () => {
+        print(Signup.render());
     },
     "/about": () => {
         print("About Page");
@@ -20,6 +28,7 @@ router.on({
         console.log(value.data.id);
         print(Detail.render(value.data.id));
     },
+    
 });
 
 router.notFound(() => print("Not Found Page"));
