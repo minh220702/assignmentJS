c1172f66b69a54ab8e0a92aa3b962be5cf65d59d
Hoang Minh <minhnhph14989@fpt.edu.vn>
2022-01-13 16:55:56 +0700

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,7 @@ const HomePage = {
     render() {
         return /* html */`
             <div class="banner">
-                <img src="../../image/Banner.jpg" />
+                <img src="https://gulfoilvietnam.vn/Data/images/Slide/Banner%2011-Manchester.jpg" />
             </div>
             <div class="news">
                 ${News.render()}
